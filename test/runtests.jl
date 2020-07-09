7a225acd30775b1b26201fddf6484ea88a21c698
Kevin Bonham <kevbonham@gmail.com>
2020-07-08 22:28:51 -0400

    Fix biosequences tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -92,38 +92,29 @@ end # strings
 #         @test gc_content(dna"ANTG") == 0.25
 #         @test gc_content(dna"cccggg") * 100 == 100.0
 #         @test gc_content(dna"ATta") == 0.0
-#         @test_throws Exception gc_content(dna"ATty")
 #     end #  composition
 
 #     @testset "composition" begin
 #         seq = rand(['A','T','G','C','N'], 20) |> join |> LongDNASeq
 #         bc = composition(seq)
 
-#         @test bc[DNA_A] == count(==, dna"A", seq)
-#         @test bc[DNA_C] == count(==, dna"C", seq)
-#         @test bc[DNA_G] == count(==, dna"G", seq)
-#         @test bc[DNA_T] == count(==, dna"T", seq)
-#         @test bc[DNA_N] == count(==, dna"N", seq)
-
-#         @test bc[DNA_A] == count(==, dna"A", seq)
-#         @test bc[DNA_C] == count(==, dna"C", seq)
-#         @test bc[DNA_G] == count(==, dna"G", seq)
-#         @test bc[DNA_T] == count(==, dna"T", seq)
-#         @test bc[DNA_N] == count(==, dna"N", seq)
+#         @test bc[DNA_A] == count(==(DNA_A), collect(seq))
+#         @test bc[DNA_C] == count(==(DNA_C), collect(seq))
+#         @test bc[DNA_G] == count(==(DNA_G), collect(seq))
+#         @test bc[DNA_T] == count(==(DNA_T), collect(seq))
+#         @test bc[DNA_N] == count(==(DNA_N), collect(seq))
 #     end #  gc_content
 
 #     @testset "complement" begin
 #         @test complement(dna"ATTAN") == dna"TAATN"
 #         @test complement(dna"gcta") == dna"CGAT"
 #         @test complement(dna"nnnnnnn") == dna"NNNNNNN"
-#         @test_throws Exception complement(dna"ABC")
 #     end #  complement
 
 #     @testset "reverse_complement" begin
 #         @test reverse_complement(dna"ATTAN") == dna"NTAAT"
 #         @test reverse_complement(dna"gcta") == dna"TAGC"
 #         @test reverse_complement(dna"nnnnnnn") == dna"NNNNNNN"
-#         @test_throws Exception reverse_complement(dna"ABC")
 #     end #  reverse_complement
 
 #     @testset "parse_fasta" begin
@@ -135,7 +126,7 @@ end # strings
 #         ex1 = parse_fasta(ex1_path)
 #         @test ex1 isa Tuple
 #         @test all(x-> x isa String, ex1[1])
-#         @test all(x-> x isa String, ex1[2])
+#         @test all(x-> x isa LongSequence, ex1[2])
 
 #         @test ex1[1] == ["ex1.1 | easy", "ex1.2 | multiline"]
 #         @test ex1[2] == [dna"AATTATAGC", dna"CGCCCCCCAGTCGGATT"]
